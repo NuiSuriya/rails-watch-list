feafaccfd6563109cf19f2f042b3187c6b4482b7
Nui Suriya <atchara.suriya@gmail.com>
2023-05-25 00:37:21 +0200

    add new seed && front end

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,11 +18,11 @@ puts 'Importing movies'
 movies = JSON.parse(URI.open(url).read)['results']
 movies.each do |movie|
   puts "Creating #{movie['title']}"
-  poster_url_base = 'http://tmdb.lewagon.com/movie/top_rated'
+  poster_url_base = 'https://image.tmdb.org/t/p/original'
   Movie.create(
     title: movie['title'],
     overview: movie['overview'],
-    poster_url: "#{poster_url_base}/#{movie['poster_path']}",
+    poster_url: "#{poster_url_base}#{movie['poster_path']}",
     rating: movie['vote_average']
   )
 end
