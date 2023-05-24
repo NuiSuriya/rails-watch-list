31c44afb39e71baa6e9f867592391d354c15f57e
Nui Suriya <atchara.suriya@gmail.com>
2023-05-24 15:31:28 +0200

    implement list basic routes && add lists controller && view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'list/index'
+  get 'list/show'
+  get 'list/new'
+  get 'list/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'lists#index'
+  resources :lists, except: [:edit,:update]
 end
